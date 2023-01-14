cdfc75f139794574b18d9a2f1512f051f77d0bba
Betsy <116031639+betsycotter@users.noreply.github.com>
2023-01-14 14:52:10 -0700

    Updates and adding files

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
-// import the data from data.js
+// import the data from data.js 12.2.4
 const tableData = data;
 
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
 
-// Simple JavaScript console.log statement
+// Simple JavaScript console.log statement 12.3.1
 function printHello() {
   console.log("Hello there!");
 }
 
+// Takes two numbers and adds them 12.3.2
+function addition(a, b) {
+  return a + b;
+}
+
+// Functions can call other functions 12.3.2
+function doubleAddition(c, d) {
+  var total = addition(c, d) * 2;
+  return total;
+}
+
+// Converted to an arrow function 12.3.3
+addition = (a, b) => a + b;
+
+doubleAddition = (c, d)
+=> addition(c, d) * 2;
+
 let friends = ["Sarah", "Greg", "Cindy", "Jeff"];
 
 // for loop examples: 
-// example 1 using friends list above: 
+// example 1 using friends list above 12.4.1: 
 function listLoop(userList) {
   for (var i = 0; i < userList.length; i++) {
     console.log(userList[i]);
   }
 }; 
 
-// example 2: 
+// example 2 12.4.2: 
 var vegetables = ["Carrots", "Peas", "Lettuce", "Tomatoes"];
 
 for (var i = 0; i < vegetables.length; i++) {
